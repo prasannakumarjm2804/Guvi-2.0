d0ac8cf5c05992d2092bd09dd67e6c33f10dedc9
PRASANNAKUMAR J M <112399618+prasannakumarjm2804@users.noreply.github.com>
2025-02-05 10:23:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u prasannakumarjm -p Docker@34#
+    docker tag test prasannakumarjm/task2
+    docker push prasannakumarjm/task2
     
